2b724d450976a7d0e65317423c27c71b29d0b7af
Pablo Samayoa <pabloenrique.sam@gmail.com>
2017-08-26 11:51:32 -0600

    Commit inicial del sistema para analisis II

---
+++ application/controllers/Primercontroller.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PrimerController extends CI_Controller {
+
+	public function index()
+	{
+		print "Hola mundo!!";
+	}
+}
---
+++ application/controllers/cuentasxcobrar/Segundocontroller.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SegundoController extends CI_Controller {
+
+	public function index()
+	{
+		print "Hola mundo!! CxC";
+	}
+}
+?>
